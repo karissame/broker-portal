6512854deb865f9a5129e5628056f201c298c722
Karissa Martin <karissame.martin@gmail.com>
2017-03-15 14:18:11 -0500

    clone from starter

---
+++ src/js/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+@connect((store)=>{
+    //the return becomes props
+    return {
+        todos:store.todos.todos
+    }
+})
+export default class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    );
+  }
+}
